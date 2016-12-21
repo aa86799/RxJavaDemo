14c71fceffaa66a1a1f39bcf67445e6bd96d093e
stone <aa86799@163.com>
2016-12-21 09:37:46 +0800

    first commit

---
+++ app/src/main/java/com/stone/rxandroid/RxJavaActivity.java
@@ -0,0 +1,96 @@
+package com.stone.rxandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.stone.rxandroid.rxjava.TestObservable;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
+
+/**
+ * desc   :
+ */
+
+public class RxJavaActivity extends Activity {
+
+    private String tag = this.getClass().getSimpleName();
+
+    TestObservable test;
+
+    Observer<String> mObserver1 = new Observer<String>() {
+
+        @Override
+        public void onCompleted() {
+            Log.d(tag, "Completed!");
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            Log.d(tag, "Error!");
+        }
+
+        @Override
+        public void onNext(String s) {
+            Log.d(tag, "Item: " + s);
+            System.out.println("onNext" + Thread.currentThread().getId());
+        }
+    };
+
+    Subscriber<String> mSubscriber1 = new Subscriber<String>() {
+        @Override
+        public void onNext(String s) {//必须实现的抽象方法
+            Log.d(tag, "Item: " + s);
+        }
+
+        @Override
+        public void onCompleted() {//必须实现的抽象方法
+            Log.d(tag, "Completed!");
+        }
+
+        @Override
+        public void onError(Throwable e) {//必须实现的抽象方法
+            Log.d(tag, "Error!");
+        }
+
+        @Override
+        public void onStart() {//事件还未发送之前被调用   非必须重写方法
+            super.onStart();
+            Log.d(tag, "onStart!");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        test = new TestObservable();
+
+//        Observable observable1 = test.testNormal();
+//        Observable observable1 = test.testJust();
+        Observable observable1 = test.testFrom();
+        test.testSubscribe(observable1);
+        observable1.subscribeOn(Schedulers.immediate());
+//        Subscription subscription = observable1.subscribe(mObserver1);//订阅并执行call
+//        subscription.unsubscribe(); //取消订阅
+
+        setContentView(R.layout.acti_main);
+        ImageView iv = (ImageView) findViewById(R.id.iv);
+        test.setImage(iv, R.drawable.a222);
+
+        test.testScheduler();
+
+        test.testMap(this);
+
+        test.testFlatMap();
+
+        test.testLift();
+
+        test.testCompose();
+    }
+}
